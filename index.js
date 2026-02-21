29c726bb95476a937b516d91db0d7c57d69f55a3
Sakshi Ghogare <ghogaresakshi24@gmail.com>
2026-02-21 22:54:09 +0530

    port change

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const port=3000;
+const port = process.env.PORT || 3000;
+
 app.listen(port,()=>{
    console.log("server is running on port "+port);
 });
